04317e2c39ddb8cc88f0c2bad863896c871321df
Sugarboogie <Sugarboogie15@yandex.ru>
2025-10-08 22:17:07 +0300

    Create 4_hw.py

---
+++ 4_hp.py
@@ -0,0 +1,22 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+
+
+rect1 = Rectangle(5, 10)
+rect2 = Rectangle(3, 7)
+rect3 = Rectangle(8, 12)
+
+
+for i, rect in enumerate([rect1, rect2, rect3], start=1):
+    print(f"Прямоугольник {i}:")
+    print(f"  Площадь: {rect.area()}")
+    print(f"  Периметр: {rect.perimeter()}")
